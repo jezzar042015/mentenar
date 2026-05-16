b13ce6f7620ae5214a9eedaf1c6bc76939d5a6bb
Jezreel Tan <jezdtanslc90@gmail.com>
2026-05-16 19:58:33 +0800

    add qr-code

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import viteQRCode from 'vite-qr'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
     tailwindcss(),
+    viteQRCode()
   ],
   resolve: {
     alias: {
