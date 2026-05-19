9cd47192ba918c1c0d041be954338a3351893c09
Jezreel Tan <jezdtanslc90@gmail.com>
2026-05-19 18:31:14 +0800

    follow up form details

--- src/components/modals/FollowupItemForm.vue
+++ src/components/modals/FollowupItemForm.vue
@@ -1,29 +1,70 @@
 <template>
-    <div @click.self="emits('close')" class="absolute top-0 left-0 z-10 h-screen w-full bg-black/60">
-        <div class="bg-white p-5 rounded-md m-3 max-w-md">
+    <div @click.self="emits('close')"
+        class="absolute top-0 left-0 z-10 h-screen w-full bg-black/60 flex items-center justify-center">
+        <div
+            class="bg-white p-5 rounded-md h-full md:max-h-[90%] max-w-md min-w-sm md:min-w-lg flex flex-col justify-between">
             <div>
-                New Follow Up Item
-            </div>
-            <div class="flex justify-between py-2">
-                <div class="w-full flex justify-center" @click="activeSection = 'props'">
-                    <span class="px-2 cursor-pointer"
-                        :class="{ 'border-b-2 border-b-amber-400': activeSection == 'props' }">Details</span>
+                <div class="border-b border-b-gray-400 uppercase mb-5 text-sm">
+                    New Follow Up Item
                 </div>
-                <div class="w-full flex justify-center" @click="activeSection = 'checklist'">
-                    <span class="px-2 cursor-pointer"
-                        :class="{ 'border-b-2 border-b-amber-400': activeSection == 'checklist' }">Checklist</span>
+                <div class="flex justify-between py-2">
+                    <div class="w-full flex justify-center" @click="activeSection = 'props'">
+                        <span class="px-4 py-1 cursor-pointer"
+                            :class="{ 'border-b-4 border-b-amber-400 text-amber-700 font-semibold': activeSection == 'props' }">Details</span>
+                    </div>
+                    <div class="w-full flex justify-center" @click="activeSection = 'checklist'">
+                        <span class="px-4 py-1 cursor-pointer"
+                            :class="{ 'border-b-4 border-b-amber-400 text-amber-700 font-semibold': activeSection == 'checklist' }">Checklist</span>
+                    </div>
                 </div>
             </div>
             <div class="min-h-100 overflow-hidden">
-                <div v-if="activeSection == 'props'">
-                    Details
+                <div v-if="activeSection == 'props'" class="py-2 px-4">
+                    <div class="mt-4 space-y-6">
+                        <div class="space-y-1">
+                            <div class="text-xs text-gray-600">Item Name</div>
+                            <input type="text" v-model="draft.task"
+                                class="py-1 px-2 border-b border-b-gray-600 w-full outline-none">
+                        </div>
+
+                        <div class="space-y-1">
+                            <div class="text-xs text-gray-600">Target</div>
+                            <input type="date" v-model="draft.target"
+                                class="py-1 px-2 border-b border-b-gray-600 w-full outline-none">
+                        </div>
+
+                        <div class="space-y-1">
+                            <div class="text-xs text-gray-600">Assignee</div>
+                            <input type="text" v-model="draft.assignees"
+                                class="py-1 px-2 border-b border-b-gray-600 w-full outline-none">
+                        </div>
+
+                        <div class="space-y-1">
+                            <div class="text-xs text-gray-600">Status</div>
+                            <input type="text" v-model="draft.status" readonly
+                                class="py-1 px-2 border-b border-b-gray-600 w-full outline-none">
+                        </div>
+
+                        <div class="space-y-1">
+                            <div class="text-xs text-gray-600">Location</div>
+                            <textarea name="" v-model="draft.location"
+                                class="py-1 px-2 border-b border-b-gray-600 w-full outline-none" rows="1"></textarea>
+                        </div>
+
+                        <div class="space-y-1">
+                            <div class="text-xs text-gray-600">Remarks</div>
+                            <textarea name="" v-model="draft.remarks"
+                                class="py-1 px-2 border-b border-b-gray-600 w-full outline-none"></textarea>
+                        </div>
+                    </div>
                 </div>
-                <div v-if="activeSection == 'checklist'">
-                    Checklist
+                <div v-if="activeSection == 'checklist'" class="py-2 px-4">
+                    
                 </div>
             </div>
-            <div>
+            <div class="space-x-2.5 pb-4">
                 <button class="shadow-md rounded-md bg-amber-400 py-2 px-3 text-sm cursor-pointer">Add Item</button>
+                <button @click="emits('close')" class="shadow-md rounded-md bg-gray-200 py-2 px-3 text-sm cursor-pointer">Cancel</button>
             </div>
         </div>
     </div>
